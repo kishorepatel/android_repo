6fdd1656514940651fd184480a873535e90797be
kishorepatel <kishore.patel.india@gmail.com>
2015-12-11 13:04:08 +0530

    Add animal sounds(raw), pics(drawable)

--- AnimalsVoiceEnglish/app/src/main/java/in/blogspot/androidoupsolving/animalsvoiceenglish/MainActivity.java
+++ AnimalsVoiceEnglish/app/src/main/java/in/blogspot/androidoupsolving/animalsvoiceenglish/MainActivity.java
@@ -4,7 +4,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Advanceable;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 
@@ -18,7 +21,12 @@ public class MainActivity extends AppCompatActivity{
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.single_item);
+
+        ImageView view = (ImageView) findViewById(R.id.img_animal);
+
+        Animation zoomin = AnimationUtils.loadAnimation(this, R.anim.image_zoom_out);
+        view.setAnimation(zoomin);
 
 //        if(convertView instanceof AdView)
 //            return convertView;
@@ -31,9 +39,11 @@ public class MainActivity extends AppCompatActivity{
 //        AdRequest adRequest = new AdRequest.Builder().build();
 //        adCur.loadAd(adRequest);
 
-//        AdView ad  = (AdView) findViewById(R.id.ads_main);
-//        AdRequest req = new AdRequest.Builder().build();
-//        ad.loadAd(req);
+        AdView ad  = (AdView) findViewById(R.id.ads_main);
+        AdRequest req = new AdRequest.Builder().build();
+        ad.loadAd(req);
+
+        view.startAnimation(zoomin);
 
     }
 }
